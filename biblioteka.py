2d3f16456aba70703cf9cc0cc97ee9f4bbc44aea
Toasteris <aurisaluzas@gmail.com>
2024-10-13 12:59:17 +0300

    pridejau galimybe prideti knygas

--- biblioteka.py
+++ biblioteka.py
@@ -37,3 +37,28 @@ def issaugoti_knyga(knyga):
 
     except Exception as e:
         print(f"nepavyko issaugoti knygos: {e}")
+
+def panaikinti_knygas():
+        try:
+            ribiniai_metai = int(input("Irasykite metu riba: "))
+            if os.path.exists(DATA_FILE):
+                with open(DATA_FILE, 'r') as file:
+                    knygos = json.load(file)
+            else:
+                print("Nera ko naikinti")
+                return
+            
+            naujos_knygos = [knyga for knyga in knygos if knyga['isleidimo_metai'] >= ribiniai_metai]
+
+            if len(knygos) == len(naujos_knygos):
+                print("Jokiu knygu nepanaikinta. Visos knygos naujesnes nei ribiniai metai")
+            else:
+                print(f"{len(knygos) - len(naujos_knygos)} knyga(os) panaikintos")
+
+            with open(DATA_FILE, 'w') as file:
+                json.dump(naujos_knygos, file, indent=4)
+
+        except ValueError:
+            print("Blogai irasyta. Metai turi buti skaicius")
+        except Exception as e:
+            print(f"Ivyko klaida: {e}")
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from biblioteka import prideti_knyga
+from biblioteka import prideti_knyga, panaikinti_knygas
 
 def main():
     while True:
         print("\nBibliotekos valdymas:")
         print("1. Prideti knyga")
-        print("2. Isjungti")
+        print("2. Panaikinti senas knygas")
+        print("3. Isjungti")
 
         pasirinkimas = input("Pasirinkite: ")
 
         if pasirinkimas == "1":
             prideti_knyga()
         elif pasirinkimas == "2":
+            panaikinti_knygas
+        elif pasirinkimas == "3":
             print("Isjungta")
             break
         else:
